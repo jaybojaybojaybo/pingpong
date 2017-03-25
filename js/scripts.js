5c602986789c119727289d6d436cfc6a185d0111
jaybojaybojaybo <jasun.feddema@gmail.com>
2017-03-25 14:06:06 -0700

    refactor front end javascript into business logic and add validation to html form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,37 @@
+var allNums = [];
+
+function writeNums(entry) {
+  for (var numbers = 1; numbers <= entry; numbers++) {
+    allNums.push(numbers);
+  }
+}
+
+tableTennis = function(newNum) {
+    if (newNum % 3 === 0 && newNum % 5 === 0) {
+      return "pingpong"
+    } else if (newNum % 3 === 0) {
+      return "ping"
+    } else if (newNum % 5 === 0) {
+      return "pong"
+    } else {
+      return newNum;
+    }
+}
+
 $(document).ready(function() {
   $("form#userNumber").submit(function(event) {
     event.preventDefault();
     var entry = $("#input").val();
-    var allNums = [];
 
-    for (var numbers = 1; numbers <= entry; numbers++) {
-      allNums.push(numbers);
-    }
+    writeNums(entry);
 
-    var newNums = allNums.map(function(newNum) {
-      if (newNum % 3 === 0 && newNum % 5 === 0) {
-        return "pingpong"
-      } else if (newNum % 3 === 0) {
-        return "ping"
-      } else if (newNum % 5 === 0) {
-        return "pong"
-      } else {
-        return newNum;
-      }
-    });
+    var newNums = allNums.map(tableTennis);
 
     newNums.forEach(function(newNum) {
       $("ul").append("<li>" + newNum + "</li>")
     });
 
     $("#output").show();
+    $("form#userNumber").trigger('reset');
   });
 });
